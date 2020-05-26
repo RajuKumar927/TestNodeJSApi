b5e1bd802806334eb957380a949cd74bef9857f5
RajuKumar927 <raj09patna@gmail.com>
2020-05-27 00:46:24 +0530

    Update routes.js

--- routes.js
+++ routes.js
@@ -2,8 +2,8 @@ const express = require('express');
 
 function eRoutes() {
     const router = express.Router();
-    var employee = require('RajuKumar927/TestNodeJSApi/repository/employee/employee.routes')(router);
-    var department = require('RajuKumar927/TestNodeJSApi/repository/department/department.routes')(router);
+    var employee = require('./TestNodeJSApi/repository/employee/employee.routes')(router);
+    var department = require('./TestNodeJSApi/repository/department/department.routes')(router);
     return router;
 }
 
