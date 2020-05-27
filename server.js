71c1c474359ff858b57430a81d6b66357a426278
RajuKumar927 <raj09patna@gmail.com>
2020-05-27 10:43:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,80 @@
 
+//Initiallising node modules
+var express = require("express");
+var bodyParser = require("body-parser");
+var sql = require("mssql");
+var app = express(); 
+
+// Body Parser Middleware
+app.use(bodyParser.json()); 
+
+//CORS Middleware
+app.use(function (req, res, next) {
+    //Enabling CORS 
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, contentType,Content-Type, Accept, Authorization");
+    next();
+});
+
+//Setting up server
+ var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+ });
+
+//Initiallising connection string
+var dbConfig = {
+    user:  'integert_laganaya',
+    password: 'sunny@#2020',
+    server: 'sg1-wsq1.a2hosting.com',
+    database: 'integert_laganaya'
+};
+
+//Function to connect to database and execute query
+var  executeQuery = function(res, query){             
+     sql.connect(dbConfig, function (err) {
+         if (err) {   
+                     console.log("Error while connecting database :- " + err);
+                     res.send(err);
+                  }
+                  else {
+                         // create Request object
+                         var request = new sql.Request();
+                         // query to the database
+                         request.query(query, function (err, res) {
+                           if (err) {
+                                      console.log("Error while querying database :- " + err);
+                                      res.send(err);
+                                     }
+                                     else {
+                                       res.send(res);
+                                            }
+                               });
+                       }
+      });           
+}
+
+//GET API
+app.get("/api/user", function(req , res){
+                var query = "select * from [Tbl_Employee]";
+                executeQuery (res, query);
+});
+
+//POST API
+ app.post("/api/user", function(req , res){
+                var query = "INSERT INTO [Tbl_Employee] (FirstName,Designation,Salary) VALUES (req.body.FirstName,req.body.Designation,req.body.Salary”);
+                executeQuery (res, query);
+});
+
+//PUT API
+ app.put("/api/user/:id", function(req , res){
+                var query = "UPDATE [user] SET Name= " + req.body.Name  +  " , Email=  " + req.body.Email + "  WHERE Id= " + req.params.id;
+                executeQuery (res, query);
+});
+
+// DELETE API
+ app.delete("/api/user /:id", function(req , res){
+                var query = "DELETE FROM [user] WHERE Id=" + req.params.id;
+                executeQuery (res, query);
+});
